032437304ed7eb2140f5bb994cbd377006c52873
sam <wenghaishi1@gmail.com>
2022-09-19 12:06:05 +0800

    minor fixes

--- contracts/Dex.sol
+++ contracts/Dex.sol
@@ -3,5 +3,14 @@
 pragma solidity ^0.8.14;
 
 contract Dex {
-    string public name = "dex";
+    string public TokenName;
+    uint256 public TokenId;
+    mapping(address => uint256) balances;
+    mapping(address => mapping (address => uint256)) allowed;
+
+    function swapToken(uint256 _tokenId, uint256 _tokenAmount) public payable {
+        require(msg.value = _tokenAmount, "wrong amount");
+
+    }
+
 }
